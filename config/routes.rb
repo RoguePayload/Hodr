c29cb8021bbe558477960f2752a1a4fc937951a5
AubreyLove <awlove@aubreylove.space>
2024-02-20 12:36:13 -0600

    Initial Repo of Hodr

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  # Set Root Path #
+  root 'static_pages#home'
+
+  # Set Static Page Paths #
+  get '/about',    to: 'static_pages#about'
+  get '/howto',    to: 'static_pages#howto'
+  get '/contact',  to: 'static_pages#contact'
+end
