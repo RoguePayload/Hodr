d5074f5eb28a324436e87767c6a3ef0d5689af28
AubreyLove <awlove@aubreylove.space>
2024-02-25 05:01:22 -0600

    Fixed image upload issue

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,7 +6,7 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { minimum: 4 }
-  validates :image, content_type: { in: %w[image/png,image/jpeg,image/jpg,image/gif,image/ani,audio/wav,audio/wave,audio/x-wav,audio/mp3,audio/mpeg],
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
                                      message: "must be a valid image format" },
                     size:         { less_than: 5.gigabytes,
                                      message:   "should be less than 5GB" }
