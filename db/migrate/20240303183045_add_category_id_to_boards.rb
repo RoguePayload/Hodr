8d79fded5f5d1c227394229fc62a33477c354fb2
AubreyLove <awlove@aubreylove.space>
2024-03-03 13:11:42 -0600

    Working on fixing error with boards

--- db/migrate/20240303183045_add_category_id_to_boards.rb
+++ db/migrate/20240303183045_add_category_id_to_boards.rb
@@ -1,12 +1,6 @@
 class AddCategoryIdToBoards < ActiveRecord::Migration[7.0]
   def change
     add_column :boards, :category_id, :integer
+    add_index :boards, :category_id
   end
-
-  def change
-    unless table_exists?(:categories)
-      add_column :boards, :category_id, :integer
-      end
-  end
-
 end
