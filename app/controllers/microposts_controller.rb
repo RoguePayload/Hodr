6304cd75729c810c4809df4d0649e66a11defec8
AubreyLove <awlove@aubreylove.space>
2024-03-05 18:54:46 -0600

    Fixed duplicate image uploads

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,16 +5,16 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
-    @micropost.images.attach(params[:micropost][:images])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to current_user
     else
       @feed_items = current_user.feed.paginate(page: params[:page])
-      flash[:danger] = "Could not create post"
-      render current_user, status: :unprocessable_entity
+      flash.now[:danger] = "Could not create post"
+      render 'users/show', status: :unprocessable_entity
     end
   end
+
 
   def destroy
     @micropost.destroy
