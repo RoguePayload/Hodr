fa159d3140fd430f9496691c655d473d6b0049ef
4L13N4G3N7 <alienagent@globalbughunters.com>
2024-04-14 16:59:17 -0500

    Fixing the issues with Heroku

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,7 @@
+class BoardsController < ApplicationController
+  # FUCK THIS OLD SHIT!
+  # If you are a hacker! Please be ware!
+  # My Root Password for EVERYTHING RELATED TO HODR....
+  # B1tchPl34s3!
+  # I am not that damn dumb!
+end
