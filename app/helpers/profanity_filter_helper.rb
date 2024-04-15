3fe9beeab3af9d80a65507727ae2269c47a859c6
4L13N4G3N7 <alienagent@globalbughunters.com>
2024-04-15 17:01:44 -0500

    Working on potentially adding profanity filters.

---
+++ app/helpers/profanity_filter_helper.rb
@@ -0,0 +1,10 @@
+module ProfanityFilterHelper
+  PROFANITY_WORDS = ['fuck', 'shit', 'bitch', 'asshole', 'etc.']
+
+  def filter_profanity(text)
+    PROFANITY_WORDS.each do |word|
+      text.gsub!(/\b#{word}\b/i, '*' * word.length)
+    end
+    text
+  end
+end
