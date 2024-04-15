374c9c54fc43944f1c1ca93dbd448b3664160aa6
4L13N4G3N7 <alienagent@globalbughunters.com>
2024-04-14 21:50:49 -0500

    Trying to fix dropdown issues with the new DOM/AJAX js

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,10 +1,4 @@
 # app/channels/chat_channel.rb
 class ChatChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "chat_chamber_#{params['chamber_id']}"
-  end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
 end
